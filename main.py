8f37b47f5f8e3dd6708198f6434724e67123c5fe
Basskind <33504331+Basskind@users.noreply.github.com>
2017-11-10 01:24:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,20 @@
-import asyncio as asyncio
 import discord
-from discord import Game, Server, Member, Embed
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
 
-import SECRETS
-import STATICS
-from commands import cmd_ping
-
-client = discord.Client()
-
-
-commands = {
-
-    "ping": cmd_ping,
-
-}
-
+client = Bot(description="Basic Bot by Habchy#1665", command_prefix="PUT YOUR PREFIX HERE", pm_help = True)
 
 @client.event
-@asyncio.coroutine
-def on_ready():
-    print("Bot is logged in successfully. Running on servers:\n")
-    for s in client.servers:
-        print("  - %s (%s)" % (s.name, s.id))
-    yield from client.change_presence(game=Game(name="Just a Test"))
+async def on_ready():
+	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    yield from client.change_presence(game=Game(name="This is just for tutorial purposes!"))
 
+@client.command()
+async def ping(*args):
 
-@client.event
-@asyncio.coroutine
-def on_message(message):
-    if message.content.startswith(STATICS.PREFIX):
-        invoke = message.content[len(STATICS.PREFIX):].split(" ")[0]
-        args = message.content.split(" ")[1:]
-        if commands.__contains__(invoke):
-            yield from commands.get(invoke).ex(args, message, client, invoke)
-        else:
-            yield from client.send_message(message.channel, embed=Embed(color=discord.Color.red(), description=("The command `%s` is not valid!" % invoke)))
+	await client.say(":ping_pong: Pong!")
+	await asyncio.sleep(3)
+	await client.say(":warning: This bot was created by **Habchy#1665**, it seems that you have not modified it yet. Go edit the file and try it out!")
 
-
-
-
-client.run(SECRETS.TOKEN)
+client.run('MzY1OTU0MDQ2MDkzMjk1NjI4.DOZ7oA.eBP_A8Bul6JL6riBMif_jNYq8Co')
