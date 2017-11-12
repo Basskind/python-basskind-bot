a6c8f5b295e0bd2c4683276347bcc4c267bb3f41
Basskind <33504331+Basskind@users.noreply.github.com>
2017-11-12 11:12:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,20 @@ async def on_ready():
 	print('--------')
 	print('Created by Habchy#1665')
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+	
 # This is a basic example of a call and response command. You tell it do "this" and it does it.
 @client.command()
 async def ping(*args):
